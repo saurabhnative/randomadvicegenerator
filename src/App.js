834e4c1792c1fc9b6bb126924e7059f2d84c5995
Saurabh Native <codeclassifiers@gmail.com>
2020-03-28 19:56:22 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [advice, updateAdvice] = useState('');
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(data => {
+          updateAdvice(data.slip.advice)
+        })
+  }
+  useEffect(() => {
+    fetchAdvice()
+  }, [''])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button learn-more" onClick={()=>fetchAdvice()}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
     </div>
   );
 }
